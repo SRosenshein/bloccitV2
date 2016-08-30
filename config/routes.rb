e9fb0f1883a6af31a3eaf6881be3c304b2e68616
Sam Rosenshein <srosenshein1@gmail.com>
2016-08-30 22:28:18 +0000

    Assignment 25: complete

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   get 'about' => 'welcome#about'
   
+  post 'users/confirm' => 'users#confirm'
+  
   root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
