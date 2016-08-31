869c32227c3e268ceeefa2fda8525a8b43a7898c
Sam Rosenshein <srosenshein1@gmail.com>
2016-08-31 20:57:17 +0000

    Assignment 26: complete3

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
     has_secure_password
     
     def avatar_url
-        gravatar_id = Digest::MD5::hexdigest(self.email).downcase
+        gravatar_id = Digest::MD5::hexdigest(email).downcase
         
         "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
     end
